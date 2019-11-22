e654a36b682ce2c3c81b5f15cc27653a47c8d8e9
Luca Baffa <luca@openbookpublishers.com>
2019-11-22 10:11:54 +0000

    sanitize URL by stripping fragment

--- src/fix_toc_xhtml.py
+++ src/fix_toc_xhtml.py
@@ -2,6 +2,7 @@
 from bs4 import BeautifulSoup
 import argparse
 import os
+from urllib.parse import urlsplit
 
 def get_h1_text(file_path):
     with open(file_path, 'r') as section:
@@ -25,6 +26,17 @@ def write_output(soup, output_file):
     with open(output_file, 'wb') as output:
         raw_soup = soup.encode('utf-8')
         output.write(raw_soup)
+
+def sanitize_href(href):
+    """
+    Strips fragment from href.
+    i.e. 'index.xhtml#my_anchor' -> 'index.xhtml'
+    
+    urlsplit(file_path)[2] returns the path attribute of a URL.
+    """
+    clean_href = urlsplit(href)[2]
+
+    return clean_href
 
 def run():
     # Parse input arguments
@@ -58,14 +70,21 @@ def run():
 
             href = link.get('href')
 
+            # Sanitize href by stripping URL fragment
+            clean_href = sanitize_href(href)
+
+            if href != clean_href:
+                link['href'] = clean_href
+                print('[INFO] {} sanitized to {}'.format(href, clean_href))
+
             # Replace strings in 'known' filenames
-            if href in known:
-                link.string.replace_with(known[href])
+            if clean_href in known:
+                link.string.replace_with(known[clean_href])
                 continue
 
             # Replace string in 'uncertain' filenames
             file_path = os.path.join(os.path.dirname(args.input_file), \
-                                     href)
+                                     clean_href)
             h1_text = get_h1_text(file_path)
 
             if h1_text:
@@ -74,7 +93,7 @@ def run():
 
             # If no match found, string is unchanged
             print("[INFO] No title found for {}; fallback" \
-                  .format(href))
+                  .format(clean_href))
 
         write_output(soup, args.output_file)
 
